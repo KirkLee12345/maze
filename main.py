e32a6f08dba16a68778981a330ca814a132a6d17
KirkLee123 <KirkLee123@foxmail.com>
2026-01-29 14:57:20 +0800

    refactor(game): 重构迷宫游戏代码结构并添加配置文件功能
    - 添加 JSON 库导入以支持配置文件读写
    - 重构 Settings 类添加 save 和 load 方法实现配置持久化
    - 为 Settings 类属性添加中文注释说明
    - 将 Block 类改为字典结构存储迷宫块状态
    - 重构 Level 类的迷宫生成算法逻辑
    - 移动并重命名 findfa 和 connect_block 函数到 Level 类内部
    - 重构 Player 类移除对 setting 对象的依赖
    - 优化 Level 类中的坐标计算和迷宫绘制方法

--- main.py
+++ main.py
@@ -1,84 +1,91 @@
 import random
 import pygame
+import json
 
 
 
 class Settings:
     def __init__(self):
-        self.screen_width = 1200
-        self.screen_height = 800
-        self.line_width = 4
-        self.block_size = 50
-        self.bg_color = (255, 255, 255)
-        self.line_color = (0, 0, 0)
-        self.fps = 60
-        self.random_seed = 0  # 设置随机数种子，设置为0则每次运行都生成一个随机数
-        self.player_default_color = (120, 0, 120)
-        self.player_default_r = 15
+        self.block_size = 50  # 方格大小
+        self.screen_width = 1200  # 屏幕宽度（必须是方格大小的整数倍）
+        self.screen_height = 800  # 屏幕高度（必须是方格大小的整数倍）
+        self.line_width = 4  # 墙壁线宽度
+        self.bg_color = (255, 255, 255)  # 背景颜色（元组RGB值）
+        self.line_color = (0, 0, 0)  # 墙壁颜色（元组RGB值）
+        self.fps = 60  # 帧率
+        self.random_seed = 0  # 随机数种子（设置为0则每次运行都生成一个随机数）
+        self.player_default_color = (120, 0, 120)  # 玩家默认颜色（元组RGB值）
+        self.player_default_r = 15  # 玩家默认半径
 
+    def save(self, path="setting.json"):
+        with open(path, "w") as f:
+            json.dump(self.__dict__, f)
 
-class Block:
-    def __init__(self, u, d, l, r):
-        self.u = u
-        self.d = d
-        self.l = l
-        self.r = r
+    def load(self, path="setting.json"):
+        with open(path, "r") as f:
+            self.__dict__ = json.load(f)
 
 
 class Level:
     def __init__(self, setting):
-        self.setting = setting
+        self.line_width = setting.line_width
+        self.width = setting.screen_width
+        self.height = setting.screen_height
+        self.block_size = setting.block_size
+        self.line_color = setting.line_color
+        self.bg_color = setting.bg_color
+        self.random_seed = setting.random_seed if setting.random_seed else random.randint(1, 100000000000000000000000000000000)
+        self.n = int(setting.screen_width / setting.block_size)
+        self.m = int(setting.screen_height / setting.block_size)
         self.blocks = []
-        for i in range(int(setting.screen_width/setting.block_size)):
+        for i in range(self.n):
             self.blocks.append([])
-            for j in range(int(setting.screen_height/setting.block_size)):
-                self.blocks[i].append(Block(False, False, False, False))
-        self.fa = {}
-        self.n = int(self.setting.screen_width / setting.block_size)
-        self.m = int(self.setting.screen_height / setting.block_size)
+            for j in range(int(self.m)):
+                self.blocks[i].append({"u": False, "d": False, "l": False, "r": False})
 
     def draw(self, screen):
-        for i in range(int(self.setting.screen_width/self.setting.block_size+1)):
-            pygame.draw.line(screen, self.setting.line_color, (i*self.setting.block_size, 0), (i*self.setting.block_size, self.setting.screen_height), self.setting.line_width)
-        for j in range(int(self.setting.screen_height/self.setting.block_size+1)):
-            pygame.draw.line(screen, self.setting.line_color, (0, j*self.setting.block_size), (self.setting.screen_width, j*self.setting.block_size), self.setting.line_width)
+        for i in range(int(self.n+1)):
+            pygame.draw.line(screen, self.line_color, (i*self.block_size, 0), (i*self.block_size, self.height), self.line_width)
+        for j in range(int(self.m+1)):
+            pygame.draw.line(screen, self.line_color, (0, j*self.block_size), (self.width, j*self.block_size), self.line_width)
         for i in range(len(self.blocks)):
             for j in range(len(self.blocks[i])):
-                if self.blocks[i][j].u:
-                    pygame.draw.line(screen, self.setting.bg_color, (i*self.setting.block_size+self.setting.line_width/2+1, j*self.setting.block_size), (i*self.setting.block_size+self.setting.block_size-self.setting.line_width/2, j*self.setting.block_size), self.setting.line_width)
-                if self.blocks[i][j].l:
-                    pygame.draw.line(screen, self.setting.bg_color, (i*self.setting.block_size, j*self.setting.block_size+self.setting.line_width/2+1), (i*self.setting.block_size, j*self.setting.block_size+self.setting.block_size-self.setting.line_width/2), self.setting.line_width)
-
-    def connect_block(self, x, y):
-        self.fa[self.findfa(x)] = self.findfa(y)
-        x1 = x % self.n
-        y1 = x // self.n
-        x2 = y % self.n
-        y2 = y // self.n
-        if x1 == x2:
-            if y1 > y2:
-                self.blocks[x1][y1].u = True
-                self.blocks[x2][y2].d = True
-            else:
-                self.blocks[x1][y1].d = True
-                self.blocks[x2][y2].u = True
-        else:
-            if x1 > x2:
-                self.blocks[x1][y1].l = True
-                self.blocks[x2][y2].r = True
-            else:
-                self.blocks[x1][y1].r = True
-                self.blocks[x2][y2].l = True
-
-    def findfa(self, x):
-        if self.fa[x] == x:
-            return x
-        return self.findfa(self.fa[x])
+                if self.blocks[i][j]["u"]:
+                    pygame.draw.line(screen, self.bg_color, (i*self.block_size+self.line_width/2+1, j*self.block_size), (i*self.block_size+self.block_size-self.line_width/2, j*self.block_size), self.line_width)
+                if self.blocks[i][j]["l"]:
+                    pygame.draw.line(screen, self.bg_color, (i*self.block_size, j*self.block_size+self.line_width/2+1), (i*self.block_size, j*self.block_size+self.block_size-self.line_width/2), self.line_width)
 
     def generate_maze(self):
+        fa = {}
+        def connect_block(x, y):
+            fa[findfa(x)] = findfa(y)
+            x1 = x % self.n
+            y1 = x // self.n
+            x2 = y % self.n
+            y2 = y // self.n
+            if x1 == x2:
+                if y1 > y2:
+                    self.blocks[x1][y1]["u"] = True
+                    self.blocks[x2][y2]["d"] = True
+                else:
+                    self.blocks[x1][y1]["d"] = True
+                    self.blocks[x2][y2]["u"] = True
+            else:
+                if x1 > x2:
+                    self.blocks[x1][y1]["l"] = True
+                    self.blocks[x2][y2]["r"] = True
+                else:
+                    self.blocks[x1][y1]["r"] = True
+                    self.blocks[x2][y2]["l"] = True
+
+        def findfa(x):
+            if fa[x] == x:
+                return x
+            return findfa(fa[x])
+
         for i in range(self.m):
             for j in range(self.n):
-                self.fa[i*self.n+j] = i*self.n+j
+                fa[i*self.n+j] = i*self.n+j
         edges = []
         for i in range(self.m):
             for j in range(self.n):
@@ -87,37 +94,42 @@ class Level:
                 if i != self.m-1:
                     edges.append((i*self.n+j, (i+1)*self.n+j))
         cnt = 0
-        if self.setting.random_seed == 0:
-            self.setting.random_seed = random.randint(1, 100000000000000000000000000000000)
-        random.seed(self.setting.random_seed)
-        print("迷宫地图生成随机数种子为：", self.setting.random_seed)
+        random.seed(self.random_seed)
         while cnt < self.m*self.n-1:
             t = random.choice(edges)
             edges.remove(t)
-            if self.findfa(t[0]) != self.findfa(t[1]):
-                self.connect_block(t[0], t[1])
+            if findfa(t[0]) != findfa(t[1]):
+                connect_block(t[0], t[1])
                 cnt += 1
+
+    def save(self, path="maze.json"):
+        with open(path, "w") as f:
+            json.dump(self.__dict__, f)
+
+    def load(self, path="maze.json"):
+        with open(path, "r") as f:
+            self.__dict__ = json.load(f)
 
 
 class Player:
     def __init__(self, setting, x, y, color=None, r=None):
-        self.setting = setting
+        self.block_size = setting.block_size
         self.color = color if color else setting.player_default_color
         self.r = r if r else setting.player_default_r
         self.x = x
         self.y = y
 
     def draw(self, screen):
-        pygame.draw.circle(screen, self.color, (self.x*self.setting.block_size+self.setting.block_size/2, self.y*self.setting.block_size+self.setting.block_size/2), self.r)
+        pygame.draw.circle(screen, self.color, (self.x*self.block_size+self.block_size/2, self.y*self.block_size+self.block_size/2), self.r)
 
     def move(self, l, direction):
-        if direction == "U" and l.blocks[self.x][self.y].u:
+        if direction == "U" and l.blocks[self.x][self.y]["u"]:
             self.y -= 1
-        if direction == "D" and l.blocks[self.x][self.y].d:
+        if direction == "D" and l.blocks[self.x][self.y]["d"]:
             self.y += 1
-        if direction == "L" and l.blocks[self.x][self.y].l:
+        if direction == "L" and l.blocks[self.x][self.y]["l"]:
             self.x -= 1
-        if direction == "R" and l.blocks[self.x][self.y].r:
+        if direction == "R" and l.blocks[self.x][self.y]["r"]:
             self.x += 1
 
 
@@ -125,10 +137,12 @@ class Player:
 def main():
     pygame.init()
     setting = Settings()
+    # setting.save()
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode((setting.screen_width + setting.line_width/2, setting.screen_height + setting.line_width/2))
     l = Level(setting)
     l.generate_maze()
+    # l.save()
     p1 = Player(setting, 0, 0)
     pygame.display.set_caption("迷宫")
 
